67925f7f2e602f134fc72f65b0cf2802a453159a
zaklopotany <marry.jane.kelly@gmail.com>
2018-04-01 18:11:50 +0100

    wszystko wpierdalam

---
+++ ios/src/com/yolo/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.yolo.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.yolo.game.MyFirstGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new MyFirstGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
